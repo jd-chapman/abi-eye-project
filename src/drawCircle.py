8b330df069c3d06e295044ffe6244b633d1b8c8d
Jack Chapman <jack.chapman@sky.com>
2022-03-05 20:09:54 +0000

    Fix scaling in drawCircle script.

--- src/drawCircle.py
+++ src/drawCircle.py
@@ -9,8 +9,8 @@ def backgroundLoop():
 
     while True:
         for i in range(0, 360):
-            xVal = sin(radians(i)) * 100
-            yVal = sin(radians(i + 90)) * 100
+            xVal = (sin(radians(i)) + 1) * 50
+            yVal = (sin(radians(i + 90)) + 1) * 50
             controller.moveXY(xVal, yVal)
 
 if __name__ == "__main__":
