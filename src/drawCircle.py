2d256b60d9b0e93073a52038e7408de6a5f5a6e8
Jack Chapman <jack.chapman@sky.com>
2022-03-05 20:07:26 +0000

    Add simple script for moving in a circle.

---
+++ src/drawCircle.py
@@ -0,0 +1,20 @@
+from turtle import back
+from MotorController import MotorController
+from threading import Thread
+from time import sleep
+from math import sin, radians
+
+def backgroundLoop():
+    controller = MotorController()
+
+    while True:
+        for i in range(0, 360):
+            xVal = sin(radians(i)) * 100
+            yVal = sin(radians(i + 90)) * 100
+            controller.moveXY(xVal, yVal)
+
+if __name__ == "__main__":
+    backgroundThread = Thread(target=backgroundLoop, daemon=True)
+    backgroundThread.start()
+
+    input("Press enter to exit.")
